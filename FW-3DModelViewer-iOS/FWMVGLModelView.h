6d48bd6d9e5bd60ef5a4ee1fb1cdec9fe1af6b33
Tim Chilvers <tim@futureworkshops.com>
2012-11-26 12:35:25 +0000

    Copy and rename layer and view classes

--- FW-3DModelViewer-iOS/FWMVGLModelView.h
+++ FW-3DModelViewer-iOS/FWMVGLModelView.h
@@ -6,13 +6,17 @@
 //  Copyright (c) 2012 Future Workshops. All rights reserved.
 //
 
+#import "FWMVGLView.h"
 
 @class GLModel;
-
-#import "FWMVGLView.h"
+@class GLImage;
 
 @interface FWMVGLModelView : FWMVGLView <UIGestureRecognizerDelegate>
 
 @property (nonatomic,retain) GLModel *model;
+@property (nonatomic, strong) GLImage *texture;
+@property (nonatomic, strong) UIColor *blendColor;
+@property (nonatomic, copy) NSArray *lights;
+@property (nonatomic, assign) CATransform3D transform;
 
 @end
